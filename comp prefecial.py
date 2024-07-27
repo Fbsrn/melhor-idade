4636a710c9cb591e071571cde6c6762eaaa3ec54
Fbsrn <fbstecnicorn@gmail.com>
2024-07-27 13:25:22 -0300

    Add files via upload

---
+++ comp prefecial.py
@@ -0,0 +1,10 @@
+mi = input("Digite idade ")
+if  mi >= "19":# a comparaçao fica pacial nao faz qualquer valor
+    print("maior idade ")
+else:print("menor")
+
+
+mi = int(input("Digite idade "))
+if  mi >= 19:# a comparaçao fica completa
+    print("maior idade ")
+else:print("menor")
